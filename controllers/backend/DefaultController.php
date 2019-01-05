9b19b50bb2951178fd91d12b34c33fbebcdba990
CansuCelebi <cansucelebi1997@gmail.com>
2019-01-06 00:55:10 +0300

    Add files via upload

---
+++ controllers/backend/DefaultController.php
@@ -0,0 +1,18 @@
+<?php
+namespace kouosl\sinavprogrami\controllers\backend;
+
+
+/**
+ * Default controller for the `sinavprogrami` module
+ */
+class DefaultController extends \kouosl\base\controllers\backend\BaseController
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('_index');
+    }
+}
